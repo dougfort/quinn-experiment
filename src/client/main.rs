31adfec562af070aa9165317e57ef69433269498
dougfort <doug.fort@gmail.com>
2021-12-27 15:48:59 -0500

    add client stub

---
+++ src/client/main.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Set the RUST_LOG, if it hasn't been explicitly defined
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "debug")
+    }
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .finish(),
+    )?;
+
+    info!("client starts");
+
+    Ok(())
+}
